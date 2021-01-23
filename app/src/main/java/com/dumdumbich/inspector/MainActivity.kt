101163a402eab4503d7cce45dfd9c27b483c5fc6
DumDumbIch <dumdumbich@gmail.com>
2021-01-23 21:52:47 +0300

    Using view binding

--- app/src/main/java/com/dumdumbich/inspector/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/inspector/MainActivity.kt
@@ -2,10 +2,17 @@ package com.dumdumbich.inspector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.dumdumbich.inspector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ui: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ui = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(ui.root)
+        ui.textView.text = "Test message"
     }
 }
